9b997293190763213f990a259d2b4301d1b27d85
tahamwf <muhammadtaha12.as@gmail.com>
2025-07-13 16:03:36 +0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'connections.php';
+require_once 'insert.php';
+?>   
+
+<form method="post" action="">
+    <h1>Registration Form</h1>
+
+<label>
+    ID:
+    <input type="text" name="id" required>
+</label>
+<br>
+<label>
+    NAME:
+    <input type="text" name="name" required>
+<label>
+<br>
+<label>
+    EMAIL:
+    <input type="email" name="email" required>
+<label>
+<br>
+<label>
+    <input type="submit" value="Submit" name="submit">
+<label>
+</form>
