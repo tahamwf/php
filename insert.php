4acfbe3ec38513596ab8fb9cf52eca5939db890d
tahamwf <muhammadtaha12.as@gmail.com>
2025-07-13 15:42:52 +0500

    Update insert.php

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'connections.php';
+if(isset($_POST['submit'])){
+$id=$_POST['id'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+
+
+$sql="INSERT INTO `students` (`id`, `name`, `email`) VALUES ('$id',$name,$email);";
+if(mysqli_query($con,$sql)){
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . mysqli_error($con);
+}
+}
+?>
